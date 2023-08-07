b3a33335d5d901cecc647e2203a79ce28f3c9023
Alaaa1 <alaa0602@hotmail.com>
2023-08-07 17:04:07 +0300

    authorizer

--- src/users/createUser/handler.ts
+++ src/users/createUser/handler.ts
@@ -1,14 +1,16 @@
 import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 import { middyfy } from "@lib/middleware";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { PublishCommand, SNSClient } from "@aws-sdk/client-sns"
+import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
+import { CognitoIdentityProviderClient, SignUpCommand } from "@aws-sdk/client-cognito-identity-provider"
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 const snsClient = new SNSClient({});
+const cognitoClient = new CognitoIdentityProviderClient({});
 
 export default middyfy(async (event) => {
-  const { name, email } = event.body;
+  const { name, email, password } = event.body;
   const command = new PutCommand({
     TableName: process.env.TABLE_NAME!,
     Item: {
@@ -23,6 +25,14 @@ export default middyfy(async (event) => {
     Message: "User Created!",
     TopicArn: process.env.SNS_ARN!
   }))
+
+  // const signupCommand = new SignUpCommand({
+  //   ClientId: process.env.ClientPool_ID!,
+  //   Username: name,
+  //   Password: password,
+  // })
+
+  // await cognitoClient.send(signupCommand);
 
   return {
     statusCode: 200,
